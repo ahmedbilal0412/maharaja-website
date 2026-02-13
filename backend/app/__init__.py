30bbe5cc7bc58fd5ab6c3c18d33e36a1d15248d9
ahmedbilal0412 <ahmedbilal.0412@gmail.com>
2026-02-13 16:01:55 +0500

    fixed __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -22,6 +22,8 @@ def _cors_origins():
         "http://127.0.0.1:5500",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://maharajabuilders.pk",
+        "http://maharajabuilders.pk"
     ]
 
 
