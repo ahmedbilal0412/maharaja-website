d276a56ac010275dbb34f7ccf7cdab0b711b4800
ahmedbilal0412 <ahmedbilal.0412@gmail.com>
2026-02-13 14:46:33 +0500

    changing from postgre to mysql

--- backend/config.py
+++ backend/config.py
@@ -10,10 +10,10 @@ if _env_path.exists():
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev-secret-change-in-production"
-    # Use DATABASE_URL for PostgreSQL, e.g. postgresql://user:password@localhost:5432/property_market
+    # Use DATABASE_URL for my-sql, e.g. mysql+pymysql://user:password@localhost:5432/property_market
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or os.environ.get(
         "SQLALCHEMY_DATABASE_URI"
-    ) or "postgresql://postgres:admin@localhost:5432/property_market"
+    ) or "mysql+pymysql://u572739771_admin:/NZddzd7pV@localhost:3306/u572739771_property_marke"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "jwt-secret-change-in-production"
     JWT_ACCESS_TOKEN_EXPIRES = 60 * 60 * 24  # 1 day
